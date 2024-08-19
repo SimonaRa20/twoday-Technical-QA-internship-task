59e5da9f4899b018f07dc379b3ea7659c9a6dd84
Simona Ragauskaitė <simonaaragauskaite@gmail.com>
2024-08-19 23:43:21 +0300

    implemented scenario one

--- OnlineShopTests/OnlineShopTests/ScenarioOneTests.cs
+++ OnlineShopTests/OnlineShopTests/ScenarioOneTests.cs
@@ -15,22 +15,156 @@ namespace OnlineShopTests
         {
             driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
-            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
         }
 
         [Test]
         public void CompleteOrderForFrankieSweatshirt()
         {
-            NavigateToMenHoodiesSection();
+            NavigatesToMenHoodiesSection_WhenSelectMen_HoodiesAndSweatshirts();
+            ChecksDisplayedNumberOfJackets_WhenPageWasOpened("12");
+            OpensSelectedProductDescription_WhenSelectProductByName("Frankie Sweatshirt");
+            UpdateSizeColorAndQuantitySelection_WhenSelectSizeColorAndQuantity("M", "Green", 1);
+            AddProductToCart_WhenSelectToAddProductToCart();
+            OpensCartAndCheckProduct_WhenSelectToCheckCart("Frankie Sweatshirt");
+            OpensCheckoutPage_WhenProceedToCheckout();
+            FillsShippingDetails_WhenCheckoutPageIsOpened("test@gmail.com", "FirstNameTest", "LastNameTest", "123 Test Street", "Test 4", "TestCity", "California", "12345", "United States", "+3705555555");
+            CompletesTheOrder_WhenFilledShippingDetails();
+            GetsSuccessMessage_WhenAssertOrderSuccess();
         }
 
-        private void NavigateToMenHoodiesSection()
+        private void NavigatesToMenHoodiesSection_WhenSelectMen_HoodiesAndSweatshirts()
         {
-            driver.Navigate().GoToUrl("https://magento.softwaretestingboard.com/");
-            var menMenu = wait.Until(ExpectedConditions.ElementToBeClickable(By.LinkText("Men")));
-            menMenu.Click();
-            var hoodiesAndSweatshirtsLink = wait.Until(ExpectedConditions.ElementToBeClickable(By.LinkText("Hoodies & Sweatshirts")));
-            hoodiesAndSweatshirtsLink.Click();
+            driver.Navigate().GoToUrl(TestUtils.BaseUrl);
+            TestUtils.WaitForElementToBeClickable(wait, By.LinkText("Men")).Click();
+            TestUtils.WaitForElementToBeClickable(wait, By.LinkText("Hoodies & Sweatshirts")).Click();
+        }
+
+        private void ChecksDisplayedNumberOfJackets_WhenPageWasOpened(string selectedNumbers)
+        {
+            var pageSizeDropdown = new SelectElement(driver.FindElement(By.Id("limiter")));
+            pageSizeDropdown.SelectByValue(selectedNumbers);
+            wait.Until(driver =>
+            {
+                var selectedValue = pageSizeDropdown.SelectedOption.GetAttribute("value");
+                return selectedValue == selectedNumbers;
+            });
+            Assert.AreEqual(selectedNumbers, pageSizeDropdown.SelectedOption.GetAttribute("value"), $"The selected value in the dropdown is not '{selectedNumbers}'.");
+        }
+
+        private void OpensSelectedProductDescription_WhenSelectProductByName(string productName)
+        {
+            TestUtils.WaitForElementToBeClickable(wait, By.LinkText(productName)).Click();
+        }
+
+        private void UpdateSizeColorAndQuantitySelection_WhenSelectSizeColorAndQuantity(string size, string color, int quantity)
+        {
+            TestUtils.WaitForElementToBeClickable(wait, By.CssSelector($".swatch-attribute.size .swatch-option[aria-label='{size}']")).Click();
+            TestUtils.WaitForElementToBeClickable(wait, By.CssSelector($".swatch-attribute.color .swatch-option[aria-label='{color}']")).Click();
+            TestUtils.FillInputField(driver, By.Id("qty"), quantity.ToString());
+        }
+
+        private void AddProductToCart_WhenSelectToAddProductToCart()
+        {
+            var addToCartButton = TestUtils.WaitForElementToBeClickable(wait, By.CssSelector(".action.tocart"));
+            addToCartButton.Click();
+
+            TestUtils.WaitForElementToHaveText(wait, By.CssSelector(".counter-number"), "1");
+
+            var updatedCartCounterText = driver.FindElement(By.CssSelector(".counter-number")).Text;
+            Assert.IsTrue(int.TryParse(updatedCartCounterText, out int itemCount) && itemCount > 0, "Cart counter value is not greater than 0.");
+        }
+
+
+        private void OpensCartAndCheckProduct_WhenSelectToCheckCart(string expectedProductName)
+        {
+            TestUtils.WaitForElementToBeClickable(wait, By.CssSelector(".action.showcart")).Click();
+
+            wait.Until(driver =>
+            {
+                var cartProductElements = driver.FindElements(By.CssSelector("strong.product-item-name"));
+                return cartProductElements.Count > 0 && cartProductElements.Any(e => e.Text.Contains(expectedProductName));
+            });
+
+            var cartProduct = driver.FindElement(By.CssSelector("strong.product-item-name"));
+            Assert.IsTrue(cartProduct.Text.Contains(expectedProductName), $"Product '{expectedProductName}' not found in the cart.");
+        }
+
+        private void OpensCheckoutPage_WhenProceedToCheckout()
+        {
+            TestUtils.WaitForElementToBeClickable(wait, By.Id("top-cart-btn-checkout")).Click();
+        }
+
+        private void FillsShippingDetails_WhenCheckoutPageIsOpened(
+    string email, string firstName, string lastName, string street, string streetLine2,
+    string city, string region, string postalCode, string country, string phoneNumber)
+        {
+            // Helper method to ensure element is visible and clickable
+            IWebElement WaitForAndGetElement(By locator)
+            {
+                var element = TestUtils.WaitForElementToBeVisible(wait, locator);
+                ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].scrollIntoView(true);", element);
+                return element;
+            }
+
+            var emailInput = WaitForAndGetElement(By.CssSelector("input#customer-email"));
+            emailInput.Clear();
+            emailInput.SendKeys(email);
+
+            var firstNameInput = WaitForAndGetElement(By.Name("firstname"));
+            firstNameInput.Clear();
+            firstNameInput.SendKeys(firstName);
+
+            var lastNameInput = WaitForAndGetElement(By.Name("lastname"));
+            lastNameInput.Clear();
+            lastNameInput.SendKeys(lastName);
+
+            var streetInput = WaitForAndGetElement(By.Name("street[0]"));
+            streetInput.Clear();
+            streetInput.SendKeys(street);
+
+            var streetInputLine2 = WaitForAndGetElement(By.Name("street[1]"));
+            streetInputLine2.Clear();
+            streetInputLine2.SendKeys(streetLine2);
+
+            var cityInput = WaitForAndGetElement(By.Name("city"));
+            cityInput.Clear();
+            cityInput.SendKeys(city);
+
+            var regionDropdown = WaitForAndGetElement(By.Name("region_id"));
+            new SelectElement(regionDropdown).SelectByText(region);
+
+            var postalCodeInput = WaitForAndGetElement(By.Name("postcode"));
+            postalCodeInput.Clear();
+            postalCodeInput.SendKeys(postalCode);
+
+            var countryDropdown = WaitForAndGetElement(By.Name("country_id"));
+            new SelectElement(countryDropdown).SelectByText(country);
+
+            var phoneNumberInput = WaitForAndGetElement(By.Name("telephone"));
+            phoneNumberInput.Clear();
+            phoneNumberInput.SendKeys(phoneNumber);
+
+            var shippingMethodRadio = WaitForAndGetElement(By.CssSelector("input[value='flatrate_flatrate']"));
+            shippingMethodRadio.Click();
+
+            var continueButton = WaitForAndGetElement(By.CssSelector("[data-role='opc-continue']"));
+            continueButton.Click();
+        }
+
+        private void CompletesTheOrder_WhenFilledShippingDetails()
+        {
+            var placeOrderButton = TestUtils.WaitForElementToBeClickable(wait, By.CssSelector("#checkout-payment-method-load button.checkout"));
+            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].scrollIntoView(true);", placeOrderButton);
+            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", placeOrderButton);
+
+            wait.Until(driver => driver.Url.Contains("/checkout/onepage/success/"));
+        }
+
+        private void GetsSuccessMessage_WhenAssertOrderSuccess()
+        {
+            var successMessage = TestUtils.WaitForElementToBeVisible(wait, By.ClassName("page-title"));
+            Assert.IsTrue(successMessage.Displayed, "The success message is not displayed.");
         }
 
         [TearDown]
---
+++ OnlineShopTests/OnlineShopTests/TestUtils.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using SeleniumExtras.WaitHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShopTests
+{
+    public static class TestUtils
+    {
+        public const string BaseUrl = "https://magento.softwaretestingboard.com/";
+
+        public static IWebElement WaitForElementToBeClickable(WebDriverWait wait, By locator)
+        {
+            return wait.Until(ExpectedConditions.ElementToBeClickable(locator));
+        }
+
+        public static IWebElement WaitForElementToBeVisible(WebDriverWait wait, By locator)
+        {
+            return wait.Until(ExpectedConditions.ElementIsVisible(locator));
+        }
+
+        public static IWebElement WaitForElementToHaveText(WebDriverWait wait, By locator, string expectedText)
+        {
+            return wait.Until(driver =>
+            {
+                var element = driver.FindElement(locator);
+                return element.Text.Contains(expectedText) ? element : null;
+            });
+        }
+
+        public static void FillInputField(IWebDriver driver, By locator, string value)
+        {
+            var element = driver.FindElement(locator);
+            element.Clear();
+            element.SendKeys(value);
+        }
+    }
+}
